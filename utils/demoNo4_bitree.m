f4fa61680b31e3526f32085be80a13bb8e5dd326
Yingfeng Yu <yuyingfeng@cueb.edu.cn>
2016-05-15 08:53:33 +0800

    add bitree demo

---
+++ utils/demoNo4_bitree.m
@@ -0,0 +1,20 @@
+clear
+%One-period Binominal-tree
+S0=30;      %Underlying Asset
+X=30;       %Strike price
+R=7/100;    %Risk-free
+T=1;        %Two-periods
+DT=0.125;   %The length of interval
+u=1+1/3;    %Upper size
+SIG=log(u); %Volatility
+FLAG=1;     %Matlab default set, when "1" means call
+Q=0;        %Dividend rate
+DIV=0;      %Dividend payment
+EXDIV=0;    %Specified div in number of periods
+
+[prices1,c_bin1] = binprice(S0,X,R,T,DT,SIG,FLAG);
+
+M=1/DT;
+C0= EuroCallBinomial(S0,X,R,T,SIG,M);
+
+
