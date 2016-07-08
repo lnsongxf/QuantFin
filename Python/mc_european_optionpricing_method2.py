057a544e7038cecb56cce917ea25b9f7c421ec56
Yingfeng Yu <yuyingfeng@cueb.edu.cn>
2016-07-08 19:49:16 +0800

    add new version of mc_euro_opt_pricing

--- Python/mc_european_optionpricing_method2.py
+++ Python/mc_european_optionpricing_method2.py
@@ -14,7 +14,10 @@ S=S0*np.exp(np.cumsum((r-0.5*sigma**2)*dt+sigma
 S[0]=S0
 
 C0=mh.exp(-r*T)*sum(np.maximum(S[-1]-K,0))/I
-print("European results=",C0)
+
+time_comsume=time()-t0
+print("European Option Price=",C0)
+print("Time used=",time_comsume,"(seconds)")
 
 
 plt.subplot(2,2,1)
